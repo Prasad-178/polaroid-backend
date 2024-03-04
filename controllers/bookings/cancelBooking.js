e2baf1abcd56ccaba46a1c9a6deb1b997c0909d3
Prasad <prasadjs178@gmail.com>
2024-03-04 22:43:58 +0530

    fixed cancel ticket

--- controllers/bookings/cancelBooking.js
+++ controllers/bookings/cancelBooking.js
@@ -3,6 +3,13 @@ const Theatre = require("../../models/theatre")
 
 const cancelBooking = async (req, res) => {
     let { email, movieName, runDate, startTiming, endTiming, ticketNumbers, location } = req.body
+    console.log(req.body)
+
+    for (let i=0; i<ticketNumbers.length; i++) {
+        ticketNumbers[i][1] = ticketNumbers[i][1] - 1
+    }
+
+    console.log(ticketNumbers)
     
     startTiming = new Date(startTiming)
     endTiming = new Date(endTiming)
@@ -10,11 +17,7 @@ const cancelBooking = async (req, res) => {
     
     let movie
     try {
-        movie = await Theatre.findOne({
-            location,
-            'movieInfo.movieName': movieName,
-            'movieInfo.timings.runDate': runDate
-        })
+        movie = await Theatre.findOne({ location: location }).exec()
     } catch (err) {
         console.log(err)
     }
@@ -25,30 +28,33 @@ const cancelBooking = async (req, res) => {
             .json({ error: "No such movie exists!" })
     }
 
-    for (let i=0; i<ticketNumbers.length; i++) {
-        console.log(ticketNumbers[i])
-        console.log(movie.movieInfo[0].timings[0].seating[ticketNumbers[i][0]][ticketNumbers[i][1]])
-        movie.movieInfo[0].timings[0].seating[ticketNumbers[i][0]][ticketNumbers[i][1]] = 1
+    for (let i=0; i<movie.movieInfo.length; i++) {
+        if (movie.movieInfo[i].movieName === movieName) {
+            console.log("found moviename")
+            for (let j=0; j<movie.movieInfo[i].timings.length; j++) {
+                // const curmovie = movie.movieInfo[i].timings[j]
+                console.log(movie.movieInfo[i].timings[j].runDate, runDate)
+                if (movie.movieInfo[i].timings[j].runDate.getTime() === runDate.getTime() && movie.movieInfo[i].timings[j].startTiming.getTime() === startTiming.getTime() && movie.movieInfo[i].timings[j].endTiming.getTime() === endTiming.getTime()) {
+                    console.log("found exact timing!")
+                    for (let k=0; k<ticketNumbers.length; k++) {
+                        console.log(ticketNumbers[k])
+                        console.log(movie.movieInfo[i].timings[j].seating[ticketNumbers[k][0]][ticketNumbers[k][1]])
+                        movie.movieInfo[i].timings[j].seating[ticketNumbers[k][0]][ticketNumbers[k][1]] = 1
+                    }
+                }
+            }
+        }
     }
 
-    let updatedMovie
     try {
-        updatedMovie = await Theatre.findOneAndUpdate({
-            location,
-            'movieInfo.movieName': movieName,
-            'movieInfo.timings.runDate': runDate
-        },
-        { $set: movie },
-        { new: true }
-        )
-    } catch (error) {
-        console.log(error)
+        await Theatre.updateOne({location: location}, movie)
+    } catch (err) {
+        console.log("movie update err")
+        console.log(err)
         return res
             .status(500)
-            .json({ error: "Internal error occurred" })
+            .json({ error: "Some internal error occurred!" })
     }
-
-    console.log(updatedMovie)
 
     let bookedTicket
     try {
